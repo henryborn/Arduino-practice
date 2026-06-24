36fa80392b1678cc9839845bb5d66b5f0b1086ad
Henry <150854434+henryborn@users.noreply.github.com>
2026-06-24 02:13:45 -0400

    Added a lot of comments, please read them

--- 3dvisualizat/Accelerometer.ino
+++ 3dvisualizat/Accelerometer.ino
@@ -20,10 +20,12 @@ const int btn = 6;
 double SoundSpeed = .0343;
 VL53L0X_RangingMeasurementData_t measure;
 
+//the following two comments are from me trying to do the math and are I believe very WRONG
+
 //z changes based on x (yaw) and z (roll)
 //y changes nased on y (pitch) and z (roll)
 
-class EmaFilter { //using this bad boy again
+class EmaFilter { //using this bad boy again, puts exponential filter between data points so if there is a massive jump in the data it won't completely ruin everything
   public:
       EmaFilter(double alpha) : m_alpha(alpha), m_lastOutput(0.0) {}
 
@@ -38,90 +40,90 @@ class EmaFilter { //using this bad boy again
 };
 
 EmaFilter emaFilter(0.125);
-EmaFilter emAFilter(0.125);
+EmaFilter emAFilter(0.125); //the objects hold the last few data points so I need two different objects that way they don't mix data
 
 void setup() {
   Wire.begin();
-  Serial.begin(115200);
-  while(!Serial);
+  Serial.begin(115200); //this is a higher Baud rate so that the things run faster, I need the code to run as fast as possible so that it can be accurate. Of course it can't run too fast but the ultrasonic sensor fixes that
+  while(!Serial); //Serial monitor would take too long to open and data would be thrown to the void causing the code to never actually run
   mpu.initialize();
 
   if(mpu.testConnection() == false){
     Serial.println("MPU6050 connection failed");
-    while(mpu.testConnection() == false);
+    while(mpu.testConnection() == false); //this way I don't have to redeploy, unplugging and plugging the arduino back in usually fixes it
   }
-  if(!tof.begin()) {
+  if(!tof.begin()) { //tof stands for time of flight, the module is called a lazar ranging module but better known as a time of flight module, in my opinion it seems to be more accurate than the ultrasonic sensor
     Serial.print("VL53L0X connection failed");
     while(!tof.begin());
   }
-  pinMode(trig, OUTPUT); 
-  pinMode(echo, INPUT); 
-  pinMode(btn, INPUT_PULLUP); //initially high but if a pullup resistor changes it goes to low
-  mpu.setDLPFMode(MPU6050_DLPF_BW_20);
+  pinMode(trig, OUTPUT); //send data to the trigger pin 
+  pinMode(echo, INPUT); //recieve data from the echo pin
+  pinMode(btn, INPUT_PULLUP); //initially high but if a pullup resistor changes it goes to low, recieve data
+  mpu.setDLPFMode(MPU6050_DLPF_BW_20); //run as fast as possible
   mpu.setXAccelOffset(19600/8 - 3200/8 + 400/8 - 400/4); //don't mind, calibration is hard, when calibrating for some reason it automatically multiplies by about 8 so I have to divide by 8
   mpu.setYAccelOffset(-5900/8 + 1600/8 - 800/8 + 100/8);
   mpu.setZAccelOffset(-10000/8 + 1800/8 - 400/8 + 100/8);
   mpu.setXGyroOffset(610/8 + 320/8 + 160/8 + 80/4);
   mpu.setYGyroOffset(-170/8 - 100/4 + 20/8);
   mpu.setZGyroOffset(50/8 + 30/4);
-  digitalWrite(trig, LOW);
-  timer = micros();
+  digitalWrite(trig, LOW); // if you see my previous code, I moved this here because it's redundant to run it every time if it is already LOW from the previous loop
+  timer = micros(); //dt isn't 0 but also makes sure that the time it takes for the loop to start is accounted for in the math
 }
 
 void loop() {
-  dt = micros() - timer;
+  dt = micros() - timer; //change in time since last loop
   timer = micros();
   tof.rangingTest(&measure, false); //checks if it's ranging properly, put true to get an actual value
-  ldy = measure.RangeMilliMeter;
-  reset = digitalRead(btn);
+  ldy = measure.RangeMilliMeter; //I lost track of variables because I kept adding and removing variables that were technically the same thing after realizing how inaccurate the mpu is so ldy stands for lazar distance y
+  reset = digitalRead(btn); //if the button is pressed reset will be LOW, else HIGH
 
-  if (reset == LOW && pressed == false) {
+  if (reset == LOW && pressed == false) { //including the boolean "pressed" so that it doesn't keep resetting while the button is pressed, idk if this was a good idea and it might actually be better if it didn't have it but for safety I'm keeping it
     pressed = true;
-    dx -= dx;
+    dx -= dx; //the same as just setting it to 0 idk why I did it this way but it made sense in my head at the time of coding it
     dy -= dy;
     dz -= dz;
   }
   else if (reset == HIGH && pressed == true) pressed = false;
 
   
-  if (ldy > 2000 || ldy < 50) ldy = steady;
+  if (ldy > 2000 || ldy < 50) ldy = steady; //the approximate minimum value of 50 comes from what it was reading when I was touch the sensor and 2000 is an arbitrary number that is less than its out of bounds return value of ~8000 but greater than any value I will probably have it read
   else {
-    steady = emaFilter.Run(ldy); // make sure there are no random huge jumps 
+    steady = emaFilter.Run(ldy); // make sure there are no random huge jumps while not allowing 0 to be added to the filter nor the same number to clutter the filter
   }
   
   if (dt != 0) {
-    vdy = ((ldy - lastldy)) / (dt / 1000);
+    vdy = ((ldy - lastldy)) / (dt / 1000); //change in distance over change in time, a simple "AROC" or average rate of change equation
   }
-  if (fabs(vdy) < .25) vdy = 0;
+  if (fabs(vdy) < .25) vdy = 0; //it jitters a lot, this removes that small bit of noise from say my slight hand shaking while testing
 
   lastvdy = vdy;
   lastldy = ldy;
   digitalWrite(trig, HIGH); //Send signals (sends 8)
-  delayMicroseconds(10);
+  delayMicroseconds(10); //necessary for the ultrasonic sensor to have time to get enough data that it knows the distance, it does not use just one data point for each value it sends to the code
   digitalWrite(trig, LOW); //Stop sending
 
-  duration = pulseIn(echo, HIGH, 20000); //the 20 ms time cap seems to censor out out of range things which is fantastic
-  usdz = SoundSpeed * duration / 2 / 100; //some math to turn time into distance
+  duration = pulseIn(echo, HIGH, 20000); //the 20 ms time cap seems to censor out out of range things which is fantastic, the final number is the cap in how long it will wait for a wave to come back in microseconds, echo is just which pin, HIGH means it's reading when ECHOs high
+  usdz = SoundSpeed * duration / 2 / 100; //some math to turn time into distance, since we know how fast the wave is traveling at
 
 
-  vdz = (usdz - lastusdz) / (dt / 1000000);
+  vdz = (usdz - lastusdz) / (dt / 1000000); //usdz stands for ultrasonic sensor distance Z, vdZ does not account for rotation
   lastusdz = usdz;
   
-  if (fabs(vdz) < .15) vdz = 0;
-  if (usdz != 0 && usdz < 2) {
+  if (fabs(vdz) < .15) vdz = 0; //a deadband to ignore slight shakes like before
+  if (usdz != 0 && usdz < 2) { //filter out 0, in reality I could probably ignore when it is less than 5 centimeters or .05 since that's about what touching it gets but when you actually touch it it jumps to its max value so
     steadily = emAFilter.Run(usdz);
   }
   
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  float virtual_x = az / 16384.0 * 9.81;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); //this gets all 6 axes that the mpu can get without a magnetometer attached to it
+  float virtual_x = az / 16384.0 * 9.81; // the division converts it into g forces then the multiplication is the approximate meters per second that one g force is turning it into the SI for acceleration
   float virtual_y = ax / 16384.0 * 9.81;
   float virtual_z = ay / 16384.0 * 9.81;
-  float real_gx = gz / 131.0;
+  float real_gx = gz / 131.0; // turns it from raw data to degrees per second
   float real_gy = gx / 131.0;
   float real_gz = gy / 131.0;
 
-  new_ax = sensor_reading(virtual_x);
-  last_ax = virtual_x;
+  new_ax = sensor_reading(virtual_x); //its just a deadband that I messed up initially and was too lazy to simplify
+  last_ax = virtual_x; //I had a reason for this I think it might have been velocity I don't remember
   virtual_x = new_ax;
   new_ay = sensor_reading(virtual_y);
   last_ay = virtual_y;
@@ -140,7 +142,7 @@ void loop() {
   real_gz = new_gz;
 
   if (fabs(real_gx) > 0) {
-    dx += (real_gx * dt / 1000000.0);
+    dx += (real_gx * dt / 1000000.0); //dt is in microseconds and I need it to be in seconds. Degrees adds the integral of the degrees per second over the time it takes for one loop to happen
   }
 
   if (fabs(real_gy) > 0) {
@@ -152,7 +154,7 @@ void loop() {
   }
 
   if (fabs(virtual_x) > 0) {
-    vx += (virtual_x * dt / 1000000.0);
+    vx += (virtual_x * dt / 1000000.0); //this was super innacurate and is left here to show I tried it
   }
   else{
     vx = 0;
@@ -162,21 +164,23 @@ void loop() {
   //I am instead bringing back the ultrasonic sensor for the left to right axis and a lazar ranging module for up and down translation. I will assume it doesn't move forward and backward unless it is rotated where I will use trigonometry to track movement in each direction. Assume a mecanum drive where it can't move up and down unless a ramp is introduced.
   //I will use the MPU6050 for acceleration and sensor delay correction but will use the 2 position sensors for position and derive them for approximate velocity
 
-  float rz = dx * PI / 180.0; // Yaw (dx)
+  float rz = dx * PI / 180.0; // Yaw (dx), converts from degrees to radians
   float rx = dy * PI / 180.0; // Roll (dy)
   float ry = dz * PI / 180.0; // Pitch (dz)
 
-  float cz = cos(rx), sz = sin(rx);
+  float cz = cos(rx), sz = sin(rx); //this is how 3D matrices work, they aren't fun to read normally and I'm not going to try to make it easier to read. Just trust that the math is about correct, might be slightly off but it's close enough.
   float cx = cos(ry), sx = sin(ry);
   float cy = cos(rz), sy = sin(rz);
 
-  calculatedx = (steady / 1000) * (cz * sy * sx - sz * cx) + steadily * (cz * sy * -cx + sz * sx);
+  calculatedx = (steady / 1000) * (cz * sy * sx - sz * cx) + steadily * (cz * sy * -cx + sz * sx); //this gets a steady position accounting for rotation and not just trusting that the sensor will always get the axis I set it to face on the breadboard
 
   calculatedy = -(steady / 1000) * (sz * sy * sx + cz * cx) + steadily * (-sz * sy * cx - cz * sx);
 
-  calculatedz = -(steady / 1000) * (cy * sx) + steadily * (cy * cx);
+  calculatedz = -(steady / 1000) * (cy * sx) + steadily * (cy * cx); //probably should have just divided by 1000 when defining steady but oh well, steady is the lazar sensor if you don't remember and steadily is the ultrasonic sensor after they are run through the EMAfilters
 
   //I got lost in the math, I think it works tho0ugh
+
+  //heres all my debugging code, it was a lot to understand exactly how it all worked
 
   /* Serial.print("vdy: "); Serial.println(vdy);
   Serial.println(steady / 1000);
@@ -205,7 +209,7 @@ void loop() {
   Serial.println(dz);
   Serial.print(", offsetx: "); */
 
-  //organized data
+  //organized data, check the other file to see why I organized it this way. It goes from position and then each derivative that I have
   Serial.print(calculatedx);
   Serial.print(",");
   Serial.print(calculatedy);
@@ -253,5 +257,7 @@ float dps(float current) {
     return current;
   }
 }
+
+//here are my sources, plus I had to learn how processing works but if you just download (many examples) or search it up its pretty self-explanatory
 //https://github.com/ElectronicCats/mpu6050/blob/master/examples/MPU6050_raw/MPU6050_raw.ino
 //https://github.com/adafruit/Adafruit_VL53L0X/blob/master/examples/vl53l0x/vl53l0x.ino
--- 3dvisualizat/visualization.pde
+++ 3dvisualizat/visualization.pde
@@ -1,4 +1,4 @@
-import processing.serial.*;
+import processing.serial.*; //imports every sublibrary from processing.serial, allows it to access the stuff I am printing to Serial in the other file which always runs on the arduino whether it is open or not
 
 Serial myPort;
 float xOffset = 0;
@@ -17,26 +17,25 @@ float ay = 0;
 float az = 0;
 
 void setup() {
-  size(800, 600, P3D); // Create an 800x600 canvas in 3D Mode
+  size(800, 600, P3D); // This is the size that it initially creates at, was good for visualizing everything but you can fullscreen and in the final product this has no meaning
   
-  // Print a list of available serial ports to your console
+  // this was to chech which usb port my arduino is, it ended up being 0 but always check it
   printArray(Serial.list());
                                                              
-  // REPLACE THE 0 WITH THE INDEX OF YOUR ARDUINO PORT FROM THE PRINTED LIST
-  String portName = Serial.list()[0];
-  myPort = new Serial(this, portName, 115200);
-  myPort.bufferUntil('\n'); // Only trigger a serial event when a new line arrives
+  String portName = Serial.list()[0]; //So it knows what the arduino is because unlike the arduino IDE this does not automatically find and connect to it
+  myPort = new Serial(this, portName, 115200); //brings the arduino into this file and makes sure the baud rate stays consistant across files
+  myPort.bufferUntil('\n'); // this basically means loop after every new line character which is why all the data is in one continuous string in the other file
 }
 
 void draw() {
-  background(50); // Dark grey background
-  lights();       // Turn on default 3D ambient lighting
+  background(50); // background, higher number means closer to white lower number means closer to black
+  lights();       // Turn on default 3D ambient lighting apparently
   
-  hint(DISABLE_DEPTH_TEST);
-  fill(255);
-  textSize(13);
-  text("Data: ", 20, 30);
-  text("X pos: " + nfc(xOffset, 2), 20, 50);
+  hint(DISABLE_DEPTH_TEST); //this stops the "3d camera frame" in order for the text to be still on the picture I can't be moving it in the same dimensions I am moving the breadboard so I need it to stop moving everything when writing the stillframe text as the code can't tell the difference
+  fill(255); //grayscale, 255 is white 0 is black
+  textSize(13); //13 pixels tall
+  text("Data: ", 20, 30); //all of this is basically just a print statement to specific points on the screen
+  text("X pos: " + nfc(xOffset, 2), 20, 50); //nfc trunkates the value to however many listed decimal points, in this case 2
   text("Y pos: " + nfc(yOffset, 2), 20, 70);
   text("Z pos: " + nfc(zOffset, 2), 20, 90);
   text("yaw: " + nfc(yaw, 2), 20, 110);
@@ -50,37 +49,34 @@ void draw() {
   text("X accel: " + nfc(ax, 2), 20, 270);
   text("Y accel: " + nfc(ay, 2), 20, 290);
   text("Z accel: " + nfc(az, 2), 20, 310);
-  text("gyro has a lot of drift, press button to reset", width/2 - (115), height - 20);
-  text("no vx is calculated, sorry", width/2 - (50), height - 40);
+  text("gyro has a lot of drift, press button to reset", width/2 - (120), height - 20); //makes sure it is centered if you go into fullscreen, the offsets were plug and chug, do not rely on them to be consistant based on a certain number of characters as some characters are smaller than others
+  text("no vx is calculated, sorry", width/2 - (70), height - 40);
   text("vy and vz are sensor relative and not adjusted for rotation", width/2 - (160), height - 60);
-  hint(ENABLE_DEPTH_TEST);
+  hint(ENABLE_DEPTH_TEST); //turn back on 3D moving, it doesn't move the text because the breadboard is actually redrawn every frame at the set offsets and the text has already been drawn for this frame
   
   
-  // Move the origin (0,0,0) to the center of the screen
+  // Move the origin (0,0,0) to the center of the screen, where the breadboard starts at
   translate(width/2, height/2, 0); 
   
-  // Apply the physical translation offsets calculated by your matrix
-  // You can multiply these by a scaling factor if the movements are too small to see
-  translate(xOffset * 200, yOffset * 200, -zOffset * 200);
-  rotateY(radians(yaw));
+  translate(xOffset * 200, yOffset * 200, -zOffset * 200); //this moves it from the origin to whatever the values are times and arbitrary number since the .1 or .2 (20 cm) movements translate to like less than a pixel and it would just look like it spins with no moving
+  rotateY(radians(yaw)); //this was guess and check, what I though the axes looked like in real life are not what they ended up actually being
   rotateZ(radians(pitch));
   rotateX(radians(roll));
   
-  // Draw a representation of your sensor board
-  fill(255, 255, 255); // Sleek blue color
-  stroke(255);       // White edges
-  box(200, 10, 60); // Width, height, depth of the virtual object
+  fill(255, 255, 255); // creates the breadboard as white
+  stroke(255);       // with white edges, though this could probably be removed honestly. I'm done writing code and testing though. "If it works don't fix it." If this were a professional thing I would remove redundancy like this though.
+  box(200, 10, 60); // Width, height, depth, guess and check to make it seem like the approximate dimensions of the physical board sitting enxt to me
 }
 
-void serialEvent(Serial myPort) {
-  String inputString = myPort.readStringUntil('\n');
+void serialEvent(Serial myPort) { //grab the data from serial
+  String inputString = myPort.readStringUntil('\n'); //reads until a new line is created. After new line character it resets the data array back to item 0
   
-  if (inputString != null) {
-    inputString = trim(inputString);
-    String[] data = split(inputString, ',');
+  if (inputString != null) { //if theres data
+    inputString = trim(inputString); //only get the actual items
+    String[] data = split(inputString, ','); //create an array of the data that is separated by every comma, so if I forgot a comma both datasets would be counted as one and az would probably print either the new line character or null depending on if this actually stops at or after the new line character
 
-    if (data.length >= 6) {
-      xOffset = float(data[0]);
+    if (data.length >= 11) { //allows for data to be collected even if up to two items are missing as if two items are missing then it's going to be something unimportant, everything important come in like 3-5 different variables. This comment probably doesn't make sense I'm writing this at 2 AM 
+      xOffset = float(data[0]); //defines each variable as that item in the data array translated into a float so if it was an int it would just throw 0s after a decimal point then store it. If it was a char then it would throw an error
       yOffset = float(data[1]);
       zOffset = float(data[2]);
       yaw = float(data[3]);
